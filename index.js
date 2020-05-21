28238b47f063f97e5608fec66ee4088c496321a0
antonio martinez baez <tono.mtzb@gmail.com>
2020-05-21 16:17:35 -0500

    task 6 complete, tests passing

--- index.js
+++ index.js
@@ -223,9 +223,20 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(obj){
+    super(obj);
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  standUp(slackChannel){
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}´s code on ${subject}`
+  }
 }
+
 
 /*
   STRETCH PROBLEM (no tests!)
